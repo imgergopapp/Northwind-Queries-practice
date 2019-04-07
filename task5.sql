0b4a5eedd4e8a75fc535897607c3243b72e743ab
Gergő Papp <imgergopapp@gmail.com>
2019-04-07 18:53:00 +0200

    Corrected task5 with DISTINCT ON & subquery

--- task5.sql
+++ task5.sql
@@ -1,5 +1,9 @@
-SELECT company_name AS Company,product_name AS Product, Price FROM (
-SELECT company_name, product_name, MAX(unit_price) AS Price FROM suppliers
-INNER JOIN products ON products.supplier_id = suppliers.supplier_id
-GROUP BY company_name, product_name) As foo
-ORDER BY Price DESC, product_name, company_name;
+SELECT * FROM(
+	SELECT DISTINCT ON (company_name) company_name AS Company, product_name AS Product, unit_price AS Price FROM suppliers 
+	INNER JOIN products ON products.supplier_id = suppliers.supplier_id
+	ORDER BY company_name, Price DESC ) As foo
+ORDER BY Price DESC, Product, Company ;
+ 
+/* DISTINCT ON : functions like a GROUP BY 
+   ORDER BY ... Price DESC : functions like a MAX aggregation 
+   Leading expressions in ORDER BY have to agree with columns in DISTINCT ON */
